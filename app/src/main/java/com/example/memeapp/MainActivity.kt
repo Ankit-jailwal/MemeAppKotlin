230ad3c38e59dfb75ed055b686179ef20aad4119
Ankit Jailwal <jailwalankit@gmail.com>
2022-04-24 01:25:30 +0530

    Network request using volley and network images using volley

--- app/src/main/java/com/example/memeapp/MainActivity.kt
+++ app/src/main/java/com/example/memeapp/MainActivity.kt
@@ -2,14 +2,41 @@ package com.example.memeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
     }
 
-    fun NextButtonHandler(view: View) {}
-    fun ShareButtonHandler(view: View) {}
+    private fun loadMeme() {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        val networkRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            { response ->
+                val url = response.getString("url")
+                Glide.with(this).load(url).into(memeImage)
+            },
+            { err -> Log.d("Error","Error: $err") })
+
+        queue.add(networkRequest)
+    }
+
+    fun NextButtonHandler(view: View) {
+        loadMeme()
+    }
+    fun ShareButtonHandler(view: View) {
+
+    }
 }
